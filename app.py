71336a816c6b4a6f32288515357cee7f6ee9815d
yzimmermann <92678727+yzimmermann@users.noreply.github.com>
2023-05-07 18:58:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, request, redirect
-import speech_recognition as sr
+import whisper
 
 app = Flask(__name__)
-
+model = whisper.load_model("base")
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -18,14 +18,27 @@ def index():
             return redirect(request.url)
 
         if file:
-            recognizer = sr.Recognizer()
-            audioFile = sr.AudioFile(file)
-            with audioFile as source:
-                data = recognizer.record(source)
-            transcript = recognizer.recognize_google(data, key=None)
+            audio = whisper.load_audio(file)
+            audio = whisper.pad_or_trim(audio)
+
+            # make log-Mel spectrogram and move to the same device as the model
+            mel = whisper.log_mel_spectrogram(audio).to(model.device)
+
+            # detect the spoken language
+            _, probs = model.detect_language(mel)
+            print(f"Detected language: {max(probs, key=probs.get)}")
+
+            # decode the audio
+            options = whisper.DecodingOptions()
+            result = whisper.decode(model, mel, options)
+            print(f"Detected language: {max(probs, key=probs.get)}")
+
+            # decode the audio
+            options = whisper.DecodingOptions()
+            transcript = whisper.decode(model, mel, options)
 
     return render_template('index.html', transcript=transcript)
 
 
-if __name__ == "__main__":
-    app.run(debug=True, threaded=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
